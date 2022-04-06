f85aa7b2bb4c8578d306ab88dd91f9e6b84d89b7
VinceBC <93348760+Vincecoorp21@users.noreply.github.com>
2022-04-06 08:07:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -114,3 +114,19 @@ function calculateAge(yearbirth, yearcount) {
     return 'You were born this very year!';
   }
 }
+
+//10. In this simple exercise, you will build a program that takes a value, integer , and returns a list of its multiples up to another value, limit .
+//If limit is a multiple of integer, it should be included as well. There will only ever be positive integers passed into the function, not consisting of 0. The limit will always be higher than the base.
+//For example, if the parameters passed are (2, 6), the function should return [2, 4, 6] as 2, 4, and 6 are the multiples of 2 up to 6. 
+//https://www.codewars.com/kata/58ca658cc0d6401f2700045f/train/javascript
+
+function findMultiples(integer, limit) {
+  let arrN = [] 
+  for (let i=integer; i<=limit; i++){
+    if(i % integer === 0){
+      arrN.push(i)
+    }
+    
+  }
+ return(arrN)
+}
